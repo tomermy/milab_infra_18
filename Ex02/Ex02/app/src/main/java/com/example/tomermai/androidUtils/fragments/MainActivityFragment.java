8ca345aeab6af97745a2dc09cb923027822f678e
tomer.maimon <tomerma81@gmail.com>
2017-11-26 19:07:29 +0200

    Implement Volley and Jsoup, without title fetch logic

--- Ex02/Ex02/app/src/main/java/com/example/tomermai/androidUtils/fragments/MainActivityFragment.java
+++ Ex02/Ex02/app/src/main/java/com/example/tomermai/androidUtils/fragments/MainActivityFragment.java
@@ -3,6 +3,7 @@ package com.example.tomermai.androidUtils.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +12,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.tomermai.androidUtils.FamilyListActivity;
 import com.example.tomermai.androidUtils.services.NotificationsService;
 import com.example.tomermai.androidUtils.R;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -28,7 +42,9 @@ public class MainActivityFragment extends Fragment {
     public MainActivityFragment() {
     }
 
-    /** Called when the user taps the families button */
+    /**
+     * Called when the user taps the families button
+     */
     public void startFiguresActivity(View view, int familyID) {
         Intent intent = new Intent(this.getActivity(), FamilyListActivity.class);
         intent.putExtra(EXTRA_FAMILY, familyID);
@@ -41,13 +57,59 @@ public class MainActivityFragment extends Fragment {
 
         mainViewFragment = inflater.inflate(R.layout.fragment_main, container, false);
 
+        // Toast
+        final TextView promptTextView = mainViewFragment.findViewById(R.id.text_prompt_home);
         Button showTextButton = mainViewFragment.findViewById(R.id.show_main_text_b);
+
+        // Game Of Thrones
         Button lannisterListButton = mainViewFragment.findViewById(R.id.lannister_button);
         Button starkListButton = mainViewFragment.findViewById(R.id.stark_button);
-        Button spamServiceButton = mainViewFragment.findViewById(R.id.start_notification_service);
-        final EditText setTimeInterval = mainViewFragment.findViewById(R.id.set_notifications_time);
 
-        final TextView promptTextView = mainViewFragment.findViewById(R.id.text_prompt_home);
+        // Notifications Service
+        final EditText setTimeInterval = mainViewFragment.findViewById(R.id.set_notifications_time);
+        Button spamServiceButton = mainViewFragment.findViewById(R.id.start_notification_service);
+
+        // Google Search
+        final RequestQueue queue = Volley.newRequestQueue(getContext());
+        final String googleSearchUrlPrefix = "https://www.google.com/search?q=";
+        final String charset = "UTF-8";
+
+        final EditText googleQueryInput = mainViewFragment.findViewById(R.id.search_google);
+        final TextView googleQueueryResult = mainViewFragment.findViewById(R.id.search_results);
+        Button googleSearchButton = mainViewFragment.findViewById(R.id.google_search_button);
+
+        googleSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Default value when encoding fails
+                String queryUrl = googleSearchUrlPrefix + "";
+                try {
+                    queryUrl = String.format("%s%s", googleSearchUrlPrefix,
+                            URLEncoder.encode(googleQueryInput.getText().toString(), charset));
+                } catch (UnsupportedEncodingException e) {
+                    Log.e("encoding_format", e.getMessage());
+                }
+
+                // Request an HTML string response from Google
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, queryUrl,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                getTitleFromHtml(response, googleQueueryResult);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        googleQueueryResult.setText("Something went wrong!");
+                    }
+                });
+
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
+            }
+        });
+
 
         showTextButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,6 +143,21 @@ public class MainActivityFragment extends Fragment {
         });
 
         return mainViewFragment;
+    }
+
+    private void getTitleFromHtml(String response, TextView googleQueueryResult) {
+        Document googleResponseHtml;
+
+        googleResponseHtml = Jsoup.parse(response);
+//        Elements results = googleResponseHtml.select("h3.r > a");
+//        if (results.size() == 0) {
+//            googleQueueryResult.setText("No results found!");
+//            return;
+//        }
+//        String firstResultTitle = results.first().text();
+        String pageTitle = googleResponseHtml.title();
+        googleQueueryResult
+                .setText(pageTitle);
     }
 
     public void startSpamActivity(long timeInterval) {
