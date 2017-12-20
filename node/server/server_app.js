d843426b8be1ce8a96ff5e3fc7985e949adb605d
tomer.maimon <tomerma81@gmail.com>
2017-12-20 17:46:17 +0200

    Create server app with time and getFile services

---
+++ node/server/server_app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const fs = require('fs')
+const app = express()
+
+app.get('/getTime', (req, res) => {
+	let date = new Date();
+	let currentTime = date.toTimeString();
+	
+	res.send('current time is: ' + currentTime);
+})
+
+app.get('/getFile', (req, res) => {
+	let ERROR_CODE = -1
+	let name = req.query.filename || ERROR_CODE;
+	if (name == ERROR_CODE) {
+		res.status(400).send("query param is missing");
+		return;
+	}
+
+	let filename = name + ".txt";
+	fs.readFile(filename, (err, content) => {
+	if (err) {
+			res.send("Can't find the file: " + filename + " on our server");
+			return;
+		}
+
+	res.send(content.toString());	
+	})
+})	
+
+app.listen(3000, () => console.log('Server app listening on port 3000!'))
