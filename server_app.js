97f1709e5de14c2a6d63fd4620a0ab44035ff4d5
tomer.maimon <tomerma81@gmail.com>
2017-12-27 13:05:23 +0200

    First version with params

--- server_app.js
+++ server_app.js
@@ -3,30 +3,26 @@ const fs = require('fs')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/getTime', (req, res) => {
-	let date = new Date();
-	let currentTime = date.toTimeString();
-	
-	res.send('current time is: ' + currentTime);
-})
-
-app.get('/getFile', (req, res) => {
+app.get('/files/:filename', (req, res) => {
 	let ERROR_CODE = -1
-	let name = req.query.filename || ERROR_CODE;
+
+	let name = req.params.filename || ERROR_CODE;
 	if (name == ERROR_CODE) {
-		res.status(400).send("query param is missing");
+		res.status(400).send("filename URL param is missing on request");
 		return;
 	}
 
-	let filename = name + ".txt";
-	fs.readFile(filename, (err, content) => {
-	if (err) {
-			res.send("Can't find the file: " + filename + " on our server");
-			return;
-		}
+	let readStream = fs.createReadStream(name);
 
-	res.send(content.toString());	
+	// Wait until the readable stream is valid before piping
+	readStream.on('open', _ => {
+		readStream.pipe(res);
+	});
+
+	// Catch errors when creating the stream
+	readStream.on('error', _ => {
+		res.status(400).send("error trying to open the file: " + name);
 	})
-})	
+})
 
 app.listen(port, () => console.log('Server app listening on port!'))
