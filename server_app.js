9971512bd46c5cc5916da9ba4062ba677f0b006c
tomer.maimon <tomerma81@gmail.com>
2018-01-01 08:21:42 +0200

    Implement setver logic, without timeZone

--- server_app.js
+++ server_app.js
@@ -4,26 +4,25 @@ const fs = require('fs')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/files/:filename', (req, res) => {
-	const ERROR_CODE = -1
-	let name = req.params.filename || ERROR_CODE;
-
-	if (name == ERROR_CODE) {
-		res.status(400).send("error reading URL param from request");
-		return;
-	}
-
-	let readStream = fs.createReadStream(name);
-
-	// Wait until the readable stream is valid before piping
-	readStream.on('open', _ => {
-		readStream.pipe(res);
+app.get('/getTime', (req, res, next) => {
+	fetchTimeForZone((err, timeJSON) => {
+		if (err) return res.error(err);
+		return res.json(timeJSON);
 	});
+});
 
-	// Catch errors when creating the stream
-	readStream.on('error', _ => {
-		res.status(400).send("error opening the file: " + name);
-	})
-})
+function fetchTimeForZone(cb) {
+	let date = new Date();
+
+	// epoch timestamp
+	let currentTime = date.getTime();
+
+	return cb(null, {
+		data: {
+			time: currentTime,
+			zone: "place_holder"
+		}
+	});
+}
 
 app.listen(port, () => console.log('Server app listening on port!'))
